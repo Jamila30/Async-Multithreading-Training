8cd18a83ea86ab16cd6ff11c8b473fd46a0138df
Jamila Ismayilzada <cemile.ismayilzade02@gmail.com>
2025-12-17 16:17:49 +0400

    added lesson 13

--- LS-13-Task-Class/Program.cs
+++ LS-13-Task-Class/Program.cs
@@ -1,4 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
 
@@ -71,7 +72,7 @@ Console.WriteLine("Hello, World!");
 
 //task1.ContinueWith(t =>
 //{
-   
+
 //    Console.WriteLine("Continue with this line");
 //});
 #endregion
